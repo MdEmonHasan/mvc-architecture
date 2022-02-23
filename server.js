d311dd3b8b15a0079d082484f9bcaf48e67ae683
Md. Emon <emonhasaneh33471@gmail.com>
2022-02-23 12:52:42 +0600

    updated app

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const EJS  = require('ejs');
 const env = require('dotenv');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 env.config();
 app.use(express.json())
 //app.use(urlencoded({extended: true})); 
